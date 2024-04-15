aff2abfda592bf8c39aabd5ae3d70834c2629452
HJJ <1916787042@qq.com>
2024-04-15 17:48:42 +0800

    Brew formula update for gitee version v0.0.4

--- gitee.rb
+++ gitee.rb
@@ -4,22 +4,22 @@
 class Gitee < Formula
   desc "Gitee CLI is a tool which interact with gitee server seamlessly via terminal"
   homepage "https://github.com/JJ-H/gitee_cli"
-  version "0.0.3"
+  version "0.0.4"
 
   # depends_on "cmake" => :build
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.3/gitee_amd64.tar.gz"
-      sha256 "c5c8d44270fd49036803fb7a61d470d04d6aabfd381c569524d4256e77a37eb1"
+      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.4/gitee_amd64.tar.gz"
+      sha256 "c94194ff98e2df6e2ebf31fb6c3e20bf41c4527c4597e9b5b96c4a8cd28d8892"
 
       def install
         bin.install "gitee"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.3/gitee_arm64.tar.gz"
-      sha256 "0ed781b874c4f2d4e55b948b81e320329b3412c7781639d295331c20de5e780e"
+      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.4/gitee_arm64.tar.gz"
+      sha256 "106d5d670b5e3627ce9d1a6bf2f89a633f75689fe1a7b455b2561e3524f35fe3"
 
       def install
         bin.install "gitee"
