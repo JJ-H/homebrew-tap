31a43d0a3a7838215d889b6466cf7d16ee6f710d
HJJ <54270760+JJ-H@users.noreply.github.com>
2024-04-20 20:02:00 +0800

    Brew formula update for gitee version v0.0.6

--- gitee.rb
+++ gitee.rb
@@ -4,22 +4,22 @@
 class Gitee < Formula
   desc "Gitee CLI is a tool which interact with gitee server seamlessly via terminal"
   homepage "https://github.com/JJ-H/gitee_cli"
-  version "0.0.5"
+  version "0.0.6"
 
   # depends_on "cmake" => :build
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.5/gitee-v0.0.5-darwin-amd64.tar.gz"
-      sha256 "a60b6fbac11159aff14c95a2e2fe0be88ad2360a68c32372007c7445356c5564"
+      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.6/gitee-v0.0.6-darwin-amd64.tar.gz"
+      sha256 "99c021fbd1f8b495dab3889714db9ff93b2c2910ab2b2502f6cfd1be13df1a56"
 
       def install
         bin.install "gitee"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.5/gitee-v0.0.5-darwin-arm64.tar.gz"
-      sha256 "97c0320ac5399560640afeff1b7153e37ffc0863d413cc00bacbb20d2017068e"
+      url "https://github.com/JJ-H/gitee_cli/releases/download/v0.0.6/gitee-v0.0.6-darwin-arm64.tar.gz"
+      sha256 "cfbe160c86cca694462e621d93ede06b6efd412173e869539ce96072eaf9f96a"
 
       def install
         bin.install "gitee"
